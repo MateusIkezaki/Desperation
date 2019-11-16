caca346acbaac1ac1a69a0bb3ad6049bdca3f780
Mateus Ikezaki <mateus-mori@hotmail.com>
2019-11-16 16:58:40 -0300

    animacao e texto de LIKE e NOPE adicionados

--- app/App.js
+++ app/App.js
@@ -1,5 +1,9 @@
+///////////////////////////////
+////CONINUAR DO VIDEO 6.11////
+//////////////////////////////
+
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Animated, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Animated, Image, PanResponder } from 'react-native';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height
 const SCREEN_WIDTH = Dimensions.get('window').width
@@ -13,6 +17,180 @@ const Profile = [
 ]
 
 export default class App extends React.Component{
+
+  constructor(){
+    super();
+    this.position = new Animated.ValueXY();
+    this.state = {
+      currentIndex: 0
+    }
+    this.rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2],
+      outputRange: ['-10deg', '0deg', '10deg'],
+      extrapolate: 'clamp'
+    })
+    this.rotateAndTranslate = {
+      transform: [{
+        rotate: this.rotate
+      },
+      ...this.position.getTranslateTransform()
+    ]
+    }
+
+    this.likeOpacity = this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH / 2],
+      outputRange: [0,0,1],
+      extrapolate:'clamp'
+    })
+
+    this.nopeOpacity = this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH / 2],
+      outputRange: [1,0,0],
+      extrapolate:'clamp'
+    })
+
+    this.nextCardOpacity = this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH / 2],
+      outputRange: [1,0,1],
+      extrapolate:'clamp'
+    })
+
+    this.nextCardScale = this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH / 2],
+      outputRange: [1,0.8,1],
+      extrapolate:'clamp'
+    })
+  }
+
+
+
+ 
+
+  componentWillMount(){
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder:(evt, gestureState) => true,
+      onPanResponderMove:(evt, gestureState) => {
+        this.position.setValue({x: gestureState.dx, y:gestureState.dy})
+      },
+      onPanResponderRelease: (evt, gestureState) =>{
+
+      }
+    })
+  }
+
+  renderProfile = () => {
+    return Profile.map((item, i) => {
+
+      if(i  < this.state.currentIndex){
+        
+        return null;
+      }
+      else if(i == this.state.currentIndex){
+
+      return(
+        <Animated.View
+          {...this.PanResponder.panHandlers}
+          key={item.id}
+          style={[
+           this.rotateAndTranslate,
+            {
+              height: SCREEN_HEIGHT -120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: 'absolute'
+            }
+        ]}
+        >
+
+          <Animated.View
+            style={{
+              opacity: this.likeOpacity,
+              transform:[{rotate: "-30deg"}],
+              position:"absolute",
+              top: 50,
+              left: 40,
+              zIndex: 1000
+            }}
+          >
+            <Text
+              style={{
+                borderWidth: 4,
+                borderColor: "green",
+                color: "green",
+                fontSize: 32,
+                fontWeight: "800",
+                padding: 10
+              }}
+            >LIKE</Text>
+          </Animated.View>
+
+          <Animated.View
+            style={{
+              opacity: this.nopeOpacity,
+              transform:[{rotate: "30deg"}],
+              position:"absolute",
+              top: 50,
+              right: 40,
+              zIndex: 1000
+            }}
+            >
+            <Text
+            style={{
+              borderWidth: 4,
+              borderColor: "red",
+              color: "red",
+              fontSize: 32,
+              fontWeight: "800",
+              padding: 10}}
+            >
+            NOPE</Text>
+          </Animated.View>
+
+          <Image
+            style = {{
+              flex: 1,
+              height: null,
+              width: null,
+              resizeMode: "cover",
+              borderRadius: 20
+            }}
+            source = {item.uri}
+          />
+        </Animated.View>
+      )
+    }
+    else{
+      return(
+        <Animated.View
+          key={item.id}
+          style={[
+            
+            {
+              opacity:this.nextCardOpacity,
+              transform: [{scale: this.nextCardScale}],
+              height: SCREEN_HEIGHT -120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: 'absolute'
+            }
+        ]}
+        >
+          <Image
+            style = {{
+              flex: 1,
+              height: null,
+              width: null,
+              resizeMode: "cover",
+              borderRadius: 20
+            }}
+            source = {item.uri}
+          />
+        </Animated.View>
+      )
+    }
+    }).reverse()
+  }
+  
   render(){
     return (
       <View style={{flex: 1}}>
@@ -21,26 +199,7 @@ export default class App extends React.Component{
         </View>
 
         <View style = {{flex: 1}}>
-          <Animated.View
-            style={[
-              {
-                height: SCREEN_HEIGHT -120,
-                width: SCREEN_WIDTH,
-                padding: 10
-              }
-          ]}
-          >
-            <Image
-              style = {{
-                flex: 1,
-                height: null,
-                width: null,
-                resizeMode: "cover",
-                borderRadius: 20
-              }}
-              source = {Profile[0].uri}
-            />
-          </Animated.View>
+          {this.renderProfile()}
         </View>
 
         <View style = {{height: 60}}>
